2098962c26dd0ef857131aec0efa5f7e3a0a2f4a
jayshree <117256975+jayshrirajan@users.noreply.github.com>
2023-03-14 12:47:16 +0530

    Add files via upload

---
+++ send-mail-using-springboot/src/main/java/com/example/sendmailusingspringboot/SendMailUsingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendmailusingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailUsingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMailUsingSpringbootApplication.class, args);
+	}
+
+}
---
+++ send-mail-using-springboot/src/test/java/com/example/sendmailusingspringboot/SendMailUsingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendmailusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailUsingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
